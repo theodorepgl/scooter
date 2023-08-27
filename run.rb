8bc90d8385fa95f40fa31e0a8c09fa00811e1c2d
theodorepgl <limlimtheo96@gmail.com>
2023-08-28 03:51:52 +0800

    Still not sure

--- run.rb
+++ run.rb
@@ -9,10 +9,32 @@
 # (Bonus) A way to keep track of information about the rented scooter, the user renting it, and the start and end times of the rental.
 # (Bonus) Introduce a pricing system where rental costs vary based on different times of the day (peak hours vs. off-peak hours). "Seasonal offer example"
 
-require './user.rb'
-require	'./card.rb'
-require './scooter.rb'
-require	'./station.rb'
+# run.rb
+
+require_relative 'user.rb'
+require_relative 'scooter.rb'
+require_relative 'station.rb'
+require_relative 'calculation.rb'
+
+# Create User
+user = User.new('John Doe', 'john.doe@example.com', 25)
+
+# Create Scooter
+scooter = Scooter.new('2022-01-01 09:00:00', '2022-01-01 11:00:00')
+
+# Create Station
+station = Station.new('Central Station', '123 Main St')
+
+# Perform Calculation
+calculation = Calculation.new(scooter.start_time, scooter.end_time)
+rental_duration = calculation.rental_duration
+
+# Output
+puts "User: #{user.name}, Email: #{user.email}, Age: #{user.age}"
+puts "Scooter Start Time: #{scooter.start_time}"
+puts "Scooter End Time: #{scooter.end_time}"
+puts "Rental Duration: #{rental_duration} seconds"
+puts "Station: #{station.name}, Address: #{station.address}"
 
 
 
